222685a50063241fef804692ae07aa780193f382
Angelpod <149606603+Angelpod@users.noreply.github.com>
2024-04-20 14:16:57 +0300

    folder
    new fail

---
+++ NodeGames/controllers/index.js
@@ -0,0 +1,10 @@
+const mainRouterContriller = require("./main");
+const defaultRouteController = require("./default");
+const gameRouteController = require("./game");
+const voteRouteController = require("./vote");
+module.exports = {
+    mainRouterContriller,
+    gameRouteController,
+    defaultRouteController,
+    voteRouteController,
+};
